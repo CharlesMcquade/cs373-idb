046ddad98324453eac98a52162974aea666e45b6
Charles Inglis <dylanmcquade@gmail.com>
2015-10-20 20:11:31 -0500

    updated views, now working on template for all pages

--- app/views.py
+++ app/views.py
@@ -4,9 +4,42 @@ from app import app
 
 
 
-
-
+# ---------
+#  index
+# ---------
 @app.route('/index')
 @app.route('/')
 def index():
 	return render_template('index.html')
+
+
+# ---------
+#  models
+# ---------
+@app.route('/models')
+def models():
+	return render_template('model.html')
+
+
+# ---------
+#  makes
+# ---------
+@app.route('/makes')
+def makes():
+	return render_template('make.html')
+
+
+# ---------
+#  engines
+# ---------
+@app.route('/engines')
+def engines():
+	return render_template('engine.html')
+
+
+# ---------
+#  about
+# ---------
+@app.route('/about')
+def about():
+	return render_template('about.html')
