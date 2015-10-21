aaacdd9fbee6cde48bf5383d7bb8a6c908b3204e
KevinNguyenn <KevinNguyenn@utexas.edu>
2015-10-20 21:52:47 -0500

    add single instance pages to flask

--- app/views.py
+++ app/views.py
@@ -43,3 +43,27 @@ def engines():
 @app.route('/about')
 def about():
 	return render_template('about.html')
+
+
+# ---------
+#  single_make
+# ---------
+@app.route('/single_make')
+def single_make():
+	return render_template('single_make.html')
+
+# ---------
+#  single_model
+# ---------
+@app.route('/single_model')
+def single_model():
+	return render_template('single_model.html')
+
+# ---------
+#  single_engine
+# ---------
+@app.route('/single_engine')
+def single_engine():
+	return render_template('single_engine.html')
+
+
