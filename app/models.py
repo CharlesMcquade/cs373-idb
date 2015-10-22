1adb170693cd29ff41492b53ecdf400d6d247c44
Derek Smith <dksmith89@gmail.com>
2015-10-22 15:37:20 -0700

    made model.html with sphinx.

--- app/models.py
+++ app/models.py
@@ -1,36 +1,27 @@
-#import os 
-#import sys
-#from sqlalchemy import db.Column, ForeignKey, Integer, String
-#from sqlalchemy.ext.declarative import declarative_base
-#from slqalchemy.orm import relationship
-#from sqlalchemy import create_engine
-#
-#Base = declarative_base()
-#
-# might not need all the above. Following tutorial
-# assuming db = SQLAlchemy
-
-# TODO: Verify Make, Model, and Engine use external PK
-
 from app import db
+"""
+Models.py defines the database models using Python classes. These classes will be transformed into MySQL tables, the variables in the class making the attributes of the table, by the Flask SQLAlchemy module.
+"""
+
+# association tables for model<>engine, and model<>type
+model_engine = db.Table('model_engine', db.metadata, db.Column('model_id', db.Integer, db.ForeignKey('model.id')), db.Column('engine_id', db.Integer, db.ForeignKey('engine.id')))
+model_type = db.Table('model_type', db.metadata, db.Column('model_id', db.Integer, db.ForeignKey('model.id')), db.Column('type_id', db.Integer, db.ForeignKey('type.id')))
+
 
 class Make(db.Model):
   """
-  Table to store Make details.
-  id: external PK used in model.make_id as FK
-  models: relationship to Model class. Each model instance will
-  have a field model.make that points back to the Make class. 
-  A model then can find its make with model.make.name although
-  no make attribute exists in the model schema.
-  make.models will return a query object which can then be used to
-  to select from since it is declared as 'dynamic'
+  Table to store vehicle make (manufacturer) details.
+  id: an external Primary Key. Used in model.make_id as a Foreign Key.
+  models: a relationship to the Model class. This allows each model instance to have a field model.make that points back to the Make class. 
+  A model then can find its make with model.make.name although no make attribute exists in the model schema. 
+  The lazy parameter of the relationship causes make.models to return a query object which can then be used to select from.
   """
   __tablename__ = 'make'
-  id = db.Column(Integer, primary_key=True)
-  name = db.Column(String(15), nullable=False)
-  hqlocation = db.Column(String(250))
-  ceo = db.Column(String(250))
-  established = db.Column(Integer)
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(15), nullable=False)
+  hqlocation = db.Column(db.String(250))
+  ceo = db.Column(db.String(250))
+  established = db.Column(db.Integer)
   models = db.relationship('Model', backref='make', lazy='dynamic')
 
   def __init__(self, id, name, hqlocation, ceo, established):
@@ -45,25 +36,24 @@ class Make(db.Model):
 
 class Model(db.Model):
   """
-  Table to store model details.
-  id: external PK used in ModelEngine and ModelType junction tables
-  to grab all engines and types that are associated with 
-  this model. 
-  make_id: FK, linked to Make.id
-  Can reference Make through relationship backref make. 
+  Table to store vehicle model details.
+  Each model can reference its make using model.make since a relationship backreference is established in the Make class.
+  The association tables model_engine and model_class declared as metadata to the database are used to link the many to many relationships to models and engines, and models and types. 
+  id: an external Primary Key. Used in model_engine and  model_type association tables.
+  make_id: a Foreign Key linked to Make.id.
   """
   __tablename__ = 'model'
-  id = db.Column(String, primary_key=True)
-  model_name = db.Columnn(String(25), nullable=False)
-  year = db.Column(Integer)
-  price = db.Column(Integer)
-  transmission = (String(250))
-  make_id = db.Column(Integer, ForeignKey('make.id'))
+  id = db.Column(db.String, primary_key=True)
+  model_name = db.Column(db.String(25), nullable=False)
+  year = db.Column(db.Integer)
+  price = db.Column(db.Integer)
+  transmission = db.Column(db.String(250))
+  make_id = db.Column(db.Integer, db.ForeignKey('make.id'))
   
   # models <-> engines relationship
-  engines = relationship("Engine", secondary=model_engine, backref="models", lazy="dynamic")
+  engines = db.relationship("Engine", secondary=model_engine, backref="models", lazy="dynamic")
   # models <-> types relationship
-  types = relationship("Type", secondary=model_type, backref="models", lazy="dynamic")
+  types = db.relationship("Type", secondary=model_type, backref="models", lazy="dynamic")
 
   def __init__(self, id, name, year, price, trans, make_id):
     self.id = id
@@ -78,18 +68,19 @@ class Model(db.Model):
 
 class Engine(db.Model):
   """
-  Table to store engine details, such as number of cylinders,
-  horse power, fule type, size in liters, etc. 
-  id: external PK, link to ModelEngine.engine_id as FK
+  Table to store engine details. 
+  The Engine model is a many to many relationship with the Model class. A vehicle model can have numberous engines and a single engine can belong to multiple vehicles. 
+  An Engine object has a models attribute that is not explicitly declared, but is established through the relationship that exists within the Model class. 
+  An Engine object can retrieve a query object of the models it pertains to by using engine.models.
   """
   __tablename__ = 'engine'
-  id = db.Column(Integer, primary_key=True)
-  engine_name = db.Column(string(250))
-  cylinders = db.Column(Integer)
-  hp = db.Column(Integer)
-  torque = db.Column(Integer)
-  size = db.Column(Integer)
-  fuel = db.Column(String(50))
+  id = db.Column(db.Integer, primary_key=True)
+  engine_name = db.Column(db.String(250))
+  cylinders = db.Column(db.Integer)
+  hp = db.Column(db.Integer)
+  torque = db.Column(db.Integer)
+  size = db.Column(db.Integer)
+  fuel = db.Column(db.String(50))
 
   def __init__(self, id, name, cyl, hp, tor, size, fuel):
     self.id = id
@@ -106,14 +97,16 @@ class Engine(db.Model):
 
 class Type(db.Model):
   """
-  Table to store type of vehicle and number of doors
-  id: auto increment PK, linked to ModelType.type_id as FK
+  Table to store type of vehicle. 
+  Vehicle type referes to the class or style of vehicle. For example: SUV, Truck, Coupe, etc.
+  id: an auto incrementing Primary Key, linked to ModelType.type_id as a Foreign Key.
   type_name: type of vehicle (SUV, Truck Convertible, etc)
+  The Type class shares a many to many relationship to the Model class and is able to reference a query object of its models using type.model.
   """
   __tablename__ = 'type'
-  id = db.Column(Integer, primary_key=True)
-  type_name = db.Column(String(25))
-  doors = db.Column(Integer)
+  id = db.Column(db.Integer, primary_key=True)
+  type_name = db.Column(db.String(25))
+  doors = db.Column(db.Integer)
 
   def __init__(self, name, doors):
     self.type_name = name
@@ -123,14 +116,9 @@ class Type(db.Model):
     return '<Type %r>' % (self.name)
 
 
-# association tables for model<>engine, and model<>type
-model_engine = Table('model_engine', db.metadata, Colummn('model_id', Integer, ForeignKey('model.id')), Column('engine_id', Integer, ForeignKey('engine.id')))
-
-model_type = Table('model_type', db.metadata, Column('model_id', Integer, ForeignKey('model.id')), Column('type_id', Integer, ForeignKey('type.id')))
-
 # |
 # V These may not be if the metadata association tables above work
-
+'''
 class ModelEngine(db.Model):
   """
   Junction table used to map the many to many relationship 
@@ -143,9 +131,9 @@ class ModelEngine(db.Model):
   ON Engine.id = ModelEngine.engine_id
   """
   __tablename__ = 'modelengine'
-  model_id = db.Column(String, ForeignKey('model.id'))
-  engine_id = db.Column(Integer, ForeignKey('engine.id'))
-  PrimaryKeyConstraint('model_id', 'engine_id', name='modelengine_id')
+  model_id = db.Column(db.String, db.ForeignKey('model.id'))
+  engine_id = db.Column(db.Integer, db.ForeignKey('engine.id'))
+  db.PrimaryKeyConstraint('model_id', 'engine_id', name='modelengine_id')
 
   def __init__(self, mid, eid):
     self.model_id = mid
@@ -164,10 +152,11 @@ class ModelType(db.Model):
   ON Type.id = ModelType.type_id
   """
   __tablename__ = "modeltype"
-  model_id = db.Column(Integer, ForeignKey('model.id'))
-  type_id = db.Column(Integer, ForeignKey('type.id'))
-  PrimaryKeyConstraint('model_id', 'engine_id', name='modeltype_id')
+  model_id = db.Column(db.Integer, db.ForeignKey('model.id'))
+  type_id = db.Column(db.Integer, db.ForeignKey('type.id'))
+  db.PrimaryKeyConstraint('model_id', 'engine_id', name='modeltype_id')
 
   def __init__(self, mid, tid):
     self.model_id = mid
     self.type_id = tid
+'''
