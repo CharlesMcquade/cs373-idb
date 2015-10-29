6f17ca86861139342f346a2f92bdfa9eee6a9de1
Derek Smith <dksmith89@gmail.com>
2015-10-29 15:41:05 -0700

    updated api

--- api.py
+++ api.py
@@ -1,9 +1,42 @@
 from app import db
 from models import Make, Model, Engine, Type
 
-def get_makes():
+def get_makes(id=None):
   d = dict()
-  for m in Make.query.all():
-    d[m] = m.json
+  if id is None:
+    for m in Make.query.all():
+      d[m] = m.json
+    return d
 
-  return d
+  return Make.query.filter_by(id=id).first().json
+
+def get_models(id=None):
+  d = dict()
+  if id is None:
+    for m in Model.query.all():
+      d[m.model_name] = m.json
+    return d
+
+  return Model.query.filter_by(id=id).first().json
+
+def get_engines(id=None):
+  d = dict()
+  if id is None:
+    for e in Engine.query.all():
+      d[e] = e.json
+    return d
+
+  return Engine.query.filter_by(id=id).first().json
+
+def get_types(id=None):
+  d = dict()
+  if id is None:
+    for t in Type.query.all():
+      d[t] = t.json
+    return d
+
+  return Type.query.filter_by(id=id).first().json
+
+
+
+
